4c2fede6a256c788416e9911e4f3b81fb3ddf13d
Michael Finn <mikeysight@gmail.com>
2020-06-02 14:51:06 -0700

    help

--- waev_project/settings.py
+++ waev_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'px9)c=)cg23siq1g-s$n4u+ov66*hmym=)0hlbkpxagqmy-keg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
