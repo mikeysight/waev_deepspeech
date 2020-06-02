f1a3877a9fdbb15e821b819c574a7fcd5faaf2c5
Michael Finn <mikeysight@gmail.com>
2020-06-02 10:44:42 -0700

    waev prep

--- waev_project/settings.py
+++ waev_project/settings.py
@@ -126,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ORIGIN_WHITELIST=('http://localhost:8000', 'http://127.0.0.1:8000')  
@@ -144,3 +148,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'http://localhost:8000/'
 
 LOGIN_URL = 'login'
+
+
