47dedc004d394bf652dea0ea906464a940f14cda
Michael Finn <mikeysight@gmail.com>
2020-07-11 15:52:36 -0700

    first commit

--- waev_project/settings.py
+++ waev_project/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "secrets/secret_key.txt"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
